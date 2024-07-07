cb44cb0343cac19be355dbfe7c80314e839f50dc
Zuri <132856427+Zurionek@users.noreply.github.com>
2024-07-07 02:43:41 +0200

    Add files via upload

---
+++ 8_Calendar_Maker/country_list.py
@@ -0,0 +1,161 @@
+"""
+Country	Code
+Albania	AL
+Algeria	DZ
+American Samoa	AS
+Andorra	AD
+Angola	AO
+Argentina	AR
+Armenia	AM
+Aruba	AW
+Australia	AU
+Austria	AT
+Azerbaijan	AZ
+Bahamas	BS
+Bahrain	BH
+Bangladesh	BD
+Barbados	BB
+Belarus	BY
+Belgium	BE
+Belize	BZ
+Bolivia	BO
+Bosnia and Herzegovina	BA
+Botswana	BW
+Brazil	BR
+Brunei	BN
+Bulgaria	BG
+Burkina Faso	BF
+Burundi	BI
+Cambodia	KH
+Cameroon	CM
+Canada	CA
+Chad	TD
+Chile	CL
+China	CN
+Colombia	CO
+Costa Rica	CR
+Croatia	HR
+Cuba	CU
+Curacao	CW
+Cyprus	CY
+Czechia	CZ
+Denmark	DK
+Djibouti	DJ
+Dominican Republic	DO
+Ecuador	EC
+Egypt	EG
+El Salvador	SV
+Estonia	EE
+Eswatini	SZ
+Ethiopia	ET
+Finland	FI
+France	FR
+Gabon	GA
+Georgia	GE
+Germany	DE
+Ghana	GH
+Greece	GR
+Greenland	GL
+Guam	GU
+Guatemala	GT
+Honduras	HN
+Hong Kong	HK
+Hungary	HU
+Iceland	IS
+India	IN
+Indonesia	ID
+Iran	IR
+Ireland	IE
+Isle of Man	IM
+Israel	IL
+Italy	IT
+Jamaica	JM
+Japan	JP
+Jersey	JE
+Jordan	JO
+Kazakhstan	KZ
+Kenya	KE
+Kuwait	KW
+Kyrgyzstan	KG
+Laos	LA
+Latvia	LV
+Lesotho	LS
+Liechtenstein	LI
+Lithuania	LT
+Luxembourg	LU
+Madagascar	MG
+Malawi	MW
+Malaysia	MY
+Maldives	MV
+Malta	MT
+Marshall Islands (the)	MH
+Mexico	MX
+Moldova	MD
+Monaco	MC
+Montenegro	ME
+Morocco	MA
+Mozambique	MZ
+Namibia	NA
+Netherlands	NL
+New Zealand	NZ
+Nicaragua	NI
+Nigeria	NG
+Northern Mariana Islands (the)	MP
+North Macedonia	MK
+Norway	NO
+Pakistan	PK
+Palau	PW
+Panama	PA
+Papua New Guinea	PG
+Paraguay	PY
+Peru	PE
+Philippines	PH
+Poland	PL
+Portugal	PT
+Puerto Rico	PR
+Romania	RO
+Russia	RU
+San Marino	SM
+Saudi Arabia	SA
+Serbia	RS
+Seychelles	SC
+Singapore	SG
+Slovakia	SK
+Slovenia	SI
+South Africa	ZA
+South Korea	KR
+Spain	ES
+Sweden	SE
+Switzerland	CH
+Taiwan	TW
+Tanzania	TZ
+Thailand	TH
+Timor Leste	TL
+Tonga	TO
+Tunisia	TN
+Turkey	TR
+Ukraine	UA
+United Arab Emirates	AE
+United Kingdom	GB
+United States Minor Outlying Islands	UM
+United States of America (the)	US
+Uruguay	UY
+Uzbekistan	UZ
+Vanuatu	VU
+Vatican City	VA
+Venezuela	VE
+Vietnam	VN
+Virgin Islands (U.S.)	VI
+Zambia	ZM
+Zimbabwe	ZW
+"""
+
+COUNTRY = ["AL", "DZ", "AS", "AD", "AO", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE", "BZ",
+"BO", "BA", "BW", "BR", "BN", "BG", "BF", "BI", "KH", "CM", "CA", "TD", "CL", "CN", "CO", "CR", "HR", "CU", "CW", "CY",
+"CZ", "DK", "DJ", "DO", "EC", "EG", "SV", "EE", "SZ", "ET", "FI", "FR", "GA", "GE", "DE", "GH", "GR", "GL", "GU", "GT",
+"HN", "HK", "HU", "IS", "IN", "ID", "IR", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KW", "KG", "LA",
+"LV", "LS", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "MT", "MH", "MX", "MD", "MC", "ME", "MA", "MZ", "NA", "NL", "NZ",
+"NI", "NG", "MP", "MK", "NO", "PK", "PW", "PA", "PG", "PY", "PE", "PH", "PL", "PT", "PR", "RO", "RU", "SM", "SA", "RS",
+"SC", "SG", "SK", "SI", "ZA", "KR", "ES", "SE", "CH", "TW", "TZ", "TH", "TL", "TO", "TN", "TR", "UA", "AE", "GB", "UM",
+"US", "UY", "UZ", "VU", "VA", "VE", "VN", "VI", "ZM", "ZW"]
+
